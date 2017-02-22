3a1124f351bafc0a35141b78a424158b0b4ed156
Ivan Buryak <fit_manyak@ngs.ru>
2017-02-23 00:53:07 +0700

    Some investigation

--- src/main/java/fit_manyak_at_ngs_dot_ru/test_tasks/ffs/FileFileSystem.java
+++ src/main/java/fit_manyak_at_ngs_dot_ru/test_tasks/ffs/FileFileSystem.java
@@ -1,12 +1,36 @@
 package fit_manyak_at_ngs_dot_ru.test_tasks.ffs;
 
+import fit_manyak_at_ngs_dot_ru.test_tasks.ffs.messages.Messages;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 /**
  * @author Ivan Buryak {@literal fit_manyak@ngs.ru}
- * Created on 19.02.2017.
+ *         Created on 19.02.2017.
  */
 
+@SuppressWarnings("WeakerAccess")
 public class FileFileSystem {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        try (FileChannel channel = FileChannel.open(Paths.get("test.ffs"),
+                StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)) {
+
+            ByteBuffer buffer = ByteBuffer.allocateDirect(2 + 1 + 1 + 1);
+            buffer.putShort((short) 0xFFF5);
+            buffer.put((byte) 0);
+            buffer.put((byte) 0);
+            buffer.put((byte) 0);
+            buffer.flip();
+
+            channel.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Messages.HELLO_WORLD);
     }
 }
---
+++ src/main/java/fit_manyak_at_ngs_dot_ru/test_tasks/ffs/messages/Messages.java
@@ -0,0 +1,13 @@
+package fit_manyak_at_ngs_dot_ru.test_tasks.ffs.messages;
+
+import java.util.ResourceBundle;
+
+/**
+ * @author Ivan Buryak {@literal fit_manyak@ngs.ru}
+ *         Created on 22.02.2017.
+ */
+public class Messages {
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(Messages.class.getName());
+
+    public static final String HELLO_WORLD = BUNDLE.getString("HELLO_WORLD");
+}
