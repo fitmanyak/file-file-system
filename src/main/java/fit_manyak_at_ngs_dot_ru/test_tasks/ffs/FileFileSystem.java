b7e45b6de48c0d4e706de95cd69ade2f0b2c194a
Ivan Buryak <fit_manyak@ngs.ru>
2017-02-22 10:56:53 +0700

    Initial commit

---
+++ src/main/java/fit_manyak_at_ngs_dot_ru/test_tasks/ffs/FileFileSystem.java
@@ -0,0 +1,12 @@
+package fit_manyak_at_ngs_dot_ru.test_tasks.ffs;
+
+/**
+ * @author Ivan Buryak {@literal fit_manyak@ngs.ru}
+ * Created on 19.02.2017.
+ */
+
+public class FileFileSystem {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!");
+    }
+}
