6a391ad64e7c3ae795bbf0c3984d036ffc0bcaff
Ivan Buryak <fit_manyak@ngs.ru>
2017-03-07 23:09:26 +0700

    Test spaces

--- src/test/java/fit_manyak_at_ngs_dot_ru/testtasks/ffs/test/FileFileSystemTest.java
+++ src/test/java/fit_manyak_at_ngs_dot_ru/testtasks/ffs/test/FileFileSystemTest.java
@@ -3,6 +3,7 @@ package fit_manyak_at_ngs_dot_ru.testtasks.ffs.test;
 import fit_manyak_at_ngs_dot_ru.testtasks.ffs.FileFileSystem;
 import fit_manyak_at_ngs_dot_ru.testtasks.ffs.FileFileSystemException;
 import fit_manyak_at_ngs_dot_ru.testtasks.ffs.IFileFileSystem;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.nio.file.Path;
@@ -15,7 +16,9 @@ import java.nio.file.Paths;
 
 public class FileFileSystemTest {
     private static final String TEST_PATH = "test.ffs";
-    private static final long TEST_SIZE = 12345678;
+    private static final long TEST_SIZE = 12345678L;
+    private static final long TEST_TOTAL_SPACE = 12249600L;
+    private static final long TEST_FREE_SPACE = 12249088L;
 
     @Test
     public void test() throws FileFileSystemException {
@@ -23,6 +26,8 @@ public class FileFileSystemTest {
         FileFileSystem.format(testPath, TEST_SIZE);
 
         try (IFileFileSystem fileFileSystem = FileFileSystem.mount(testPath)) {
+            Assert.assertEquals("Wrong total space", TEST_TOTAL_SPACE, fileFileSystem.getTotalSpace());
+            Assert.assertEquals("Wrong free space", TEST_FREE_SPACE, fileFileSystem.getFreeSpace());
         }
     }
 }
